aa38c5466dde683a940af9b4a407da9839ee1b95
Joshua <joshua.silveira92@gmail.com>
2019-11-25 20:54:10 -0500

    Add project files.

---
+++ dbDemo/List.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="List.aspx.cs" Inherits="dbDemo.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h1>TENANT LIST</h1>
+    <div id="result" class="_table" runat="server"> 
+        <div class="listitem">
+            <div class="col4">FIRST NAME</div>
+            <div class="col4">LAST NAME</div>
+            <div class="col4">Phone number</div>
+            <div class="col4last">EMAIL</div>
+        </div>
+
+    </div> 
+   
+</asp:Content>
---
+++ dbDemo/List.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace dbDemo
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var db = new PROPERTYDB();
+
+            string query = "select * from tenants";
+
+            List<Dictionary<String, String>> rs = db.List_Query(query);
+            foreach (Dictionary<String, String> row in rs)
+            {
+                result.InnerHtml += "<div class=\"listitem\">";
+
+                string tenantid = row["tenantid"];
+
+                string tenantfirstname = row["tenantfname"];
+                result.InnerHtml += "<div class=\"col4\">" + tenantfirstname + "</div>";
+
+                string tenantlastname = row["tenantlname"];
+                result.InnerHtml += "<div class=\"col4\">" + tenantlastname + "</div>";
+
+                string tenantphone = row["tenantphone"];
+                result.InnerHtml += "<div class=\"col4\">" + tenantphone + "</div>";
+
+                string tenantemail = row["tenantemail"];
+                result.InnerHtml += "<div class=\"col4last\">" + tenantemail + "</div>";
+
+                result.InnerHtml += "</div>";
+            }
+        }
+    }
+}
